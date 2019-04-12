6b44e9f64e4a44bc0522fbf0cfc5e8cb7d6da14a
thesseyren <sseyren@hotmail.com>
2019-04-12 19:01:41 +0300

    CurrencyDialog: seçili kurları dikkate alma eklendi

--- CurrencyListDialog.cs
+++ CurrencyListDialog.cs
@@ -6,13 +6,25 @@ namespace CashFlow
 {
     public partial class CurrencyListDialog : Gtk.Dialog
     {
-        public List<Currencies> CurrencySelections = new List<Currencies>();
+        public List<Currencies> CurrencySelections;
 
         private CheckButton[] CheckButtons;
 
         private Array CurrencyArray;
 
+        public CurrencyListDialog(List<Currencies> currentSelections)
+        {
+            CurrencySelections = currentSelections;
+            InitDialog();
+        }
+
         public CurrencyListDialog()
+        {
+            CurrencySelections = new List<Currencies>();
+            InitDialog();
+        }
+
+        private void InitDialog()
         {
             this.Build();
 
@@ -23,6 +35,9 @@ namespace CashFlow
             for (uint i = 0; i < (uint)CurrencyArray.Length; i++)
             {
                 CheckButtons[i] = new CheckButton { Label = CurrencyArray.GetValue(i).ToString() };
+                if ( CurrencySelections.Contains((Currencies)i) )
+                    CheckButtons[i].Active = true;
+
                 uint row = i / 6;
                 uint column = i % 6;
                 MainTable.Attach(CheckButtons[i], row, row + 1, column, column + 1);
@@ -33,6 +48,8 @@ namespace CashFlow
 
         protected void OnResponse(object o, ResponseArgs args)
         {
+            CurrencySelections.Clear();
+
             for (int i = 0; i < CheckButtons.Length; i++)
             {
                 if (CheckButtons[i].Active)
