301d12362746f22b2d894733617d1ba21b87e40b
thesseyren <sseyren@hotmail.com>
2019-04-19 18:52:02 +0300

    TestPlotModel kaldırıldı

--- MainWindow.cs
+++ MainWindow.cs
@@ -49,8 +49,9 @@ namespace CashFlow
                 IntervalType = DateTimeIntervalType.Days,
                 StringFormat = "dd-MM\nyyyy",
                 MajorGridlineStyle = LineStyle.Solid,
-                Minimum = DateTimeAxis.ToDouble(new DateTime(2018, 1, 1)),
-                Maximum = DateTimeAxis.ToDouble(new DateTime(2018, 9, 1))
+                Minimum = DateTimeAxis.ToDouble(Fetcher.StartAt),
+                Maximum = DateTimeAxis.ToDouble(Fetcher.EndAt),
+                MinimumMajorStep = 1
             });
             MainPlotModel.Axes.Add(new LinearAxis
             {
@@ -209,54 +210,6 @@ namespace CashFlow
         {
             Thread thread = new Thread(new ThreadStart(FetchThread));
             thread.Start();
-        }
-
-
-
-
-        protected class Item
-        {
-            public DateTime X { get; set; }
-            public double Y { get; set; }
-        }
-
-        protected static PlotModel TestPlotModel()
-        {
-            var tmp = new PlotModel { Title = "Test" };
-            tmp.Axes.Add(new LinearAxis
-            {
-                Position = AxisPosition.Left,
-                MajorGridlineStyle = LineStyle.Solid,
-                MinorGridlineStyle = LineStyle.Dot,
-                TickStyle = TickStyle.Outside
-            });
-
-            var dt = new DateTime(2010, 1, 1);
-            tmp.Axes.Add(new DateTimeAxis
-            {
-                Position = AxisPosition.Bottom,
-                Minimum = DateTimeAxis.ToDouble(dt),
-                Maximum = DateTimeAxis.ToDouble(dt.AddDays(1)),
-                IntervalType = DateTimeIntervalType.Hours,
-                MajorGridlineStyle = LineStyle.Solid,
-                Angle = 90,
-                StringFormat = "HH:mm",
-                MajorStep = 1.0 / 24 / 2, // 1/24 = 1 hour, 1/24/2 = 30 minutes
-                IsZoomEnabled = true,
-                MaximumPadding = 0,
-                MinimumPadding = 0,
-                TickStyle = TickStyle.None
-            });
-
-            var ls = new LineSeries { Title = "Line1", DataFieldX = "X", DataFieldY = "Y" };
-            var ii = new List<Item>();
-
-            for (int i = 0; i < 24; i++)
-                ii.Add(new Item { X = dt.AddHours(i), Y = i * i });
-            ii.Add(new Item { X = dt.AddHours(26), Y = 800 });
-            ls.ItemsSource = ii;
-            tmp.Series.Add(ls);
-            return tmp;
         }
     }
 }
