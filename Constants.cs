c602bfeee0ac6bba75dc22c9cfb5ae06d12bcff5
thesseyren <sseyren@hotmail.com>
2019-04-21 19:32:25 +0300

    Sabitler 'Constants.cs' altına taşındı

---
+++ Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CashFlow
+{
+    public enum Currencies
+    {
+        AUD, BGN, BRL, CAD, CHF, CNY, CZK, DKK, EUR, GBP, HKD, HRK, HUF, IDR, ILS, INR, ISK, JPY, KRW, MXN, MYR, NOK, NZD, PHP, PLN, RON, RUB, SEK, SGD, THB, TRY, USD, ZAR
+    }
+
+    public class Node
+    {
+        public DateTime Time { get; set; }
+        public double Value { get; set; }
+    }
+}
--- CurrencyFetcher.cs
+++ CurrencyFetcher.cs
@@ -6,17 +6,6 @@ using System.Net;
 
 namespace CashFlow
 {
-    public enum Currencies
-    {
-        AUD, BGN, BRL, CAD, CHF, CNY, CZK, DKK, EUR, GBP, HKD, HRK, HUF, IDR, ILS, INR, ISK, JPY, KRW, MXN, MYR, NOK, NZD, PHP, PLN, RON, RUB, SEK, SGD, THB, TRY, USD, ZAR
-    }
-
-    public class Node
-    {
-        public DateTime Time { get; set; }
-        public double Value { get; set; }
-    }
-
     public class DateException : Exception
     {
         public override string Message => "Geçerli bir tarih aralığı girilmeli.";
