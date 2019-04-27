203a005f53b172eaf19d30d81ea635b5ef521765
thesseyren <sseyren@hotmail.com>
2019-04-27 23:09:33 +0300

    ExportDialog eklendi

---
+++ ExportDialog.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Gtk;
+
+namespace CashFlow
+{
+    public enum ExportType
+    {
+        PDF, PNG, SVG
+    }
+
+    public class ExportProperties
+    {
+        public ExportType Type;
+        public double Height, Width;
+        public byte[] Color;
+    }
+
+    public partial class ExportDialog : Gtk.Dialog
+    {
+        public ExportProperties Properties;
+
+        public ExportDialog(ExportType type)
+        {
+            this.Build();
+
+            foreach (ExportType option in Enum.GetValues(typeof(ExportType)))
+                ExportTypeSelection.AppendText(option.ToString());
+            ExportTypeSelection.Active = (int)type;
+        }
+
+        protected void OnResponse(object o, ResponseArgs args)
+        {
+            if (args.ResponseId == ResponseType.Ok)
+            {
+                Properties = new ExportProperties
+                {
+                    Type = (ExportType)ExportTypeSelection.Active,
+                    Height = HeightInput.Value,
+                    Width = WidthInput.Value,
+                    Color = new byte[3]
+                    {
+                        (byte)ColorButton.Color.Red,
+                        (byte)ColorButton.Color.Green,
+                        (byte)ColorButton.Color.Blue
+                    }
+                };
+            }
+        }
+    }
+}
